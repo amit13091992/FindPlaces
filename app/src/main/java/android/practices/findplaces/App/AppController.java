f6ec73cbb1079bd0ec0c04bf415610a866b8fb85
amit13091992 <amit13091992@gmail.com>
2019-03-16 02:14:37 +0530

    Restructured project and updated files and structure.

---
+++ app/src/main/java/android/practices/findplaces/App/AppController.java
@@ -0,0 +1,73 @@
+package android.practices.findplaces.App;
+
+import android.practices.findplaces.Utils.VolleySingleton;
+import android.practices.findplaces.receivers.ConnectivityReceiver;
+import android.practices.findplaces.receivers.GPSConnectionReceiver;
+import android.support.multidex.MultiDexApplication;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Amit on 16-Mar-19.
+ */
+
+public class AppController extends MultiDexApplication {
+
+    public static VolleySingleton volleyQueueInstance;
+    private static String TAG = AppController.class.getSimpleName();
+    private static AppController mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        instantiateVolleyQueue();
+    }
+
+    public void instantiateVolleyQueue() {
+        volleyQueueInstance = VolleySingleton.getInstance(getApplicationContext());
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
+    public void setConnectivityListener(ConnectivityReceiver.ConnectivityReceiverListener listener) {
+        ConnectivityReceiver.connectivityReceiverListener = listener;
+    }
+
+    public void setGpsConnectivityListener(GPSConnectionReceiver.GPSConnectivityReceiverListener listener) {
+        GPSConnectionReceiver.gpsConnectivityReceiverListener = listener;
+    }
+}
